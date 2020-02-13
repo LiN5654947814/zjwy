1e200a6814c4f379a5fbaf2ca14ed303ae54c6af
Lin <564947814@qq.com>
2020-02-13 22:20:06 +0800

    新增车位完成

--- src/components/parking/addParking.vue
+++ src/components/parking/addParking.vue
@@ -10,19 +10,23 @@
         </el-form-item>
         <!-- 车位类型 -->
         <el-form-item label="车位类型:">
-          <el-input style="width:300px;"
-                    v-model="parkingInfo.parkingType"></el-input>
-        </el-form-item>
-        <!-- 车位位置 -->
-        <el-form-item label="车位位置:">
-          <el-input style="width:300px;"
-                    v-model="parkingInfo.parkingLocation"></el-input>
+          <el-select v-model="parkingInfo.parkingType"
+                     placeholder="请选择"
+                     disabled>
+            <el-option v-for="item in parkingTypeSelect"
+                       :key="item.value"
+                       :label="item.value"
+                       :value="item.value">
+            </el-option>
+          </el-select>
         </el-form-item>
         <!-- 车位租赁开始时间 -->
         <el-form-item label="车位租赁开始时间:">
           <el-date-picker v-model="parkingInfo.parkingStartTime"
                           type="date"
-                          placeholder="选择日期">
+                          value-format="yyyy-MM-dd"
+                          placeholder="选择日期"
+                          :disabled="isAdd">
           </el-date-picker>
 
         </el-form-item>
@@ -30,18 +34,22 @@
         <el-form-item label="车位租赁结束时间:">
           <el-date-picker v-model="parkingInfo.parkingEndTime"
                           type="date"
-                          placeholder="选择日期">
+                          value-format="yyyy-MM-dd"
+                          placeholder="选择日期"
+                          :disabled="isAdd">
           </el-date-picker>
         </el-form-item>
         <!-- 所属业主 -->
         <el-form-item label="所属业主:">
           <el-input v-model="parkingInfo.parkingOwner"
-                    style="width:300px;"></el-input>
+                    style="width:300px;"
+                    :disabled="isAdd"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="danger"
                      @click="goBack">返回</el-button>
-          <el-button type="success">新增</el-button>
+          <el-button type="success"
+                     @click="addParking">新增</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -57,12 +65,44 @@ export default {
   data () {
     return {
       title: '新增车位信息',
-      parkingInfo: []
+      parkingInfo: {
+        parkingType: '公有'
+      },
+      parkingTypeSelect: [
+        {
+          'value': '公有'
+        },
+        {
+          'value': '私有'
+        }
+      ],
+      isAdd: true
     }
   },
   methods: {
     goBack () {
-      this.$router.push('/parking')
+      this.$router.push('/parkingApplication')
+    },
+    // 新增车位信息
+    addParking () {
+      this.$axios.post('/addParking', {
+        params: {
+          parkingInfo: this.parkingInfo
+        }
+      }).then(res => {
+        if (res.data.state === 200) {
+          this.$message({
+            type: 'success',
+            message: res.data.message
+          })
+          this.$router.push({ name: 'parkingApplication' })
+        } else if (res.data.state === 401) {
+          this.$message({
+            type: 'error',
+            message: res.data.message
+          })
+        }
+      })
     }
   }
 }
